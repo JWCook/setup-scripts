02239c6711b2475e6c29bf81e23fc5247e4080c8
Jordan Cook <jordan.cook.git@proton.me>
2022-11-07 16:06:22 -0600

    Split setup scripts into a separate repo

--- fish/completions/ssc.fish
+++
@@ -1,1 +0,0 @@
-complete --command ssc --arguments "(env _SSC_COMPLETE=complete-fish COMMANDLINE=(commandline -cp) ssc)" -f
--- ipython/profile_default/startup/install_rich.py
+++
@@ -1,5 +0,0 @@
-try:
-    from rich import pretty
-    pretty.install()
-except ImportError:
-    pass
